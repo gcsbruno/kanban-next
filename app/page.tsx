e9244a091a716f7fda5f2c47706530366b9397a8
Bruno <brunoguimaraes.cs@gmail.com>
2023-09-19 18:05:30 -0300

    dark shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Task from '../components/Task'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import { Card } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import Steps from '../components/Steps'
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 
@@ -60,8 +60,8 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="bg-blue-500 text-white py-4 px-6">
+    <div className="min-h-screen">
+      <div className="py-4 px-6">
         <h1 className="text-4xl font-extrabold tracking-tight text-center">Kanban App</h1>
       </div>
       <main className="container mx-auto p-4 flex-grow">
@@ -74,15 +74,15 @@ export default function Home() {
                   {tasks
                     .filter((task) => task.status === step)
                     .map((task, taskIndex) => (
-                      <Card key={taskIndex} className="p-4 m-4 bg-white">
+                      <Card key={taskIndex} className="p-4 m-4">
                         <h3 className="text-lg font-semibold">{task.title}</h3>
                         <p>{task.description}</p>
                       </Card>
                     ))}
                 </div>
                 <Button
-                  variant="secondary"
-                  className="gap-1 m-4 hover:bg-slate-200"
+                  variant="default"
+                  className="gap-1 m-4 rounded"
                   onClick={() => addTask(index)}
                 >
                   Add Task
@@ -93,24 +93,24 @@ export default function Home() {
         </div>
       </main>
       {showDialog && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-                <Card className="bg-white p-4 rounded shadow">
-                  <h2 className="text-lg font-semibold">Create a task</h2>
-                  <Input
-                    type="text"
-                    placeholder="Title"
-                    value={newTask.title}
-                    onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-                  />
-                  <Textarea
-                    placeholder="Description"
-                    value={newTask.description}
-                    onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-                  />
-                  <Button onClick={createTask}>Create</Button>
-                  <Button onClick={closeDialog}>Cancel</Button>
-                </Card>
-              </div>
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50">
+          <Card className=" p-4 rounded shadow">
+            <h2 className="text-lg font-semibold">Create a task</h2>
+            <Input
+              type="text"
+              placeholder="Title"
+              value={newTask.title}
+              onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+            />
+            <Textarea
+              placeholder="Description"
+              value={newTask.description}
+              onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+            />
+            <Button onClick={createTask}>Create</Button>
+            <Button onClick={closeDialog}>Cancel</Button>
+          </Card>
+        </div>
       )}
     </div>
   );
--- components/Status.tsx
+++ components/Status.tsx
@@ -7,7 +7,7 @@ interface StatusProps {
 
 const Status: React.FC<StatusProps> = ({ title, children }) => {
     return (
-        <div className="bg-gray-200 p-4">
+        <div className="p-4">
             <h2 className="text-lg font-semibold">{title}</h2>
             <div>{children}</div>
         </div>
